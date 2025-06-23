aac05918418f0eeafc21dfed01e777824642803c
Sam <samscolari@icloud.com>
2025-06-23 12:12:48 -0500

    Update quest.ts

--- apps/bot/platforms/discord/embeds/quest.ts
+++ apps/bot/platforms/discord/embeds/quest.ts
@@ -14,8 +14,8 @@ export function QuestEmbed(props: {
 }) {
     return Embed({
         title: props.quest.name,
-        description: `Earns **${props.quest.xp}xp** and **${props.quest.points} ${props.quest.pointsLabel}**`,
-        image: props.quest.image,
+        description: `Earns **${props.quest.xp}xp**${props.quest.points > 0 ? ` and **${props.quest.points} ${props.quest.pointsLabel}**` : ""}`,
+        image: `${props.quest.image}?img-width=1200&img-height=800&img-fit=cover&img-onerror=redirect`,
         url: `https://nouns.gg/quests/${props.quest.id}`,
     });
 }
